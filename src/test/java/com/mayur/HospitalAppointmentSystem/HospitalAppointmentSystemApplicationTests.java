f94b9bc8a6145168e6829f73d405b241df35cd25
mayur <mayurpatil5703@gmail.com>
2025-07-21 14:13:03 +0530

    Project setup completed

---
+++ src/test/java/com/mayur/HospitalAppointmentSystem/HospitalAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayur.HospitalAppointmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
